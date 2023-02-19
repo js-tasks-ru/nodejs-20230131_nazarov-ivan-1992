b7a551c8b33bcad2580acae7bc2373fad40b5201
IvaN <nazarov.ivan.1992@list.ru>
2023-02-19 17:07:41 +0300

    add tasks 1, 2, 3, 5 from module 3

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,30 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.stringPart = ''
   }
 
   _transform(chunk, encoding, callback) {
+    chunk = chunk.toString();
+    let lines = chunk.split(os.EOL);
+
+    if (lines.length === 1){
+      this.stringPart += chunk;
+    } else {
+      lines[0] = this.stringPart + lines[0];
+      this.stringPart = lines.at(-1)
+      lines.slice(0, -1).forEach(el => {
+        this.push(el);
+      })
+    }
+    callback();
   }
 
   _flush(callback) {
+    if (this.stringPart) {
+      this.push(this.stringPart);
+    }
+    callback();
   }
 }
 
