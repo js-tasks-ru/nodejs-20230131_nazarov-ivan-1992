9ed8db2550db4a3dd2eabf787ace514ee8b556c7
IvaN <nazarov.ivan.1992@list.ru>
2023-03-11 14:54:17 +0300

    6.2

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -3,5 +3,6 @@ const mapCategory  = require('../mappers/category')
 
 module.exports.categoryList = async function categoryList(ctx, next) {
   let categories = await Category.find()
-  ctx.body = {categories: mapCategory(categories)};
+  categories = categories.map(el => mapCategory(el))
+  ctx.body = { categories };
 };
--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,19 +1,34 @@
 const Product = require('../models/Product');
+const mapProduct = require('../mappers/product')
+const mongoose = require('mongoose');
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const { subcategory } = ctx.query;
 
   if (!subcategory) return next();
-  let product = await Product.find({ subcategories: subcategory })
-  console.log(product)
-  ctx.body = ctx
+  let products = await Product.find({ subcategory: subcategory });
+  products = products.map(el => mapProduct(el))
+  ctx.body = { products };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  let products = await Product.find()
+  products = products.map(el => mapProduct(el))
+  ctx.body = { products };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  let productId = ctx.params.id;
+  if (!mongoose.isValidObjectId(productId)) {
+    ctx.throw(400, 'Invalid ObjectId');
+  }
+
+  let product = await Product.find({ _id: productId });
+ 
+  if (product.length === 0) {
+    ctx.throw(404, 'Not Found');
+  }
+
+  ctx.body = { product: mapProduct(product[0]) };
 };
 
