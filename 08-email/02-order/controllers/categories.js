e9a33ab563731e28ae5024c0373649a81adb0e1d
IvaN <nazarov.ivan.1992@list.ru>
2023-04-08 12:00:15 +0300

    delete 08/02

--- 08-email/02-order/controllers/categories.js
+++
@@ -1,7 +0,0 @@
-const Category = require('../models/Category');
-const mapCategory = require('../mappers/category');
-
-module.exports.categoryList = async function categoryList(ctx, next) {
-  const categories = await Category.find();
-  ctx.body = {categories: categories.map(mapCategory)};
-};
--- 08-email/02-order/controllers/orders.js
+++
@@ -1,8 +0,0 @@
-const Order = require('../models/Order');
-const sendMail = require('../libs/sendMail');
-
-module.exports.checkout = async function checkout(ctx, next) {
-};
-
-module.exports.getOrdersList = async function ordersList(ctx, next) {
-};
